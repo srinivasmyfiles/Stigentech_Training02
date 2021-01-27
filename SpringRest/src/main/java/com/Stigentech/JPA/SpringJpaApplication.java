71aab23d2a490299d95b2feb339cb258d7350128
srinivasmyfiles <kapasrinivas9@gmail.com>
2021-01-27 13:35:34 +0530

    rest

---
+++ SpringRest/src/main/java/com/Stigentech/JPA/SpringJpaApplication.java
@@ -0,0 +1,17 @@
+package com.Stigentech.JPA;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringJpaApplication {
+
+	public static void main(String[] args) {
+			SpringApplication.run(SpringJpaApplication.class, args);
+		
+	}
+}
